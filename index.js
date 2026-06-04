a806174ec162f711b86e46f4c3430071107e241f
sourovmmoysanju <sourovmmoysanju@gmail.com>
2026-06-05 02:22:59 +0600

    make basic setup for backend, install express, cors, mongodb , nodemon , dotenv

--- index.js
+++ index.js
@@ -2,14 +2,50 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-console.log(`Hello ${process.env.HELLO}`);
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const database = client.db("pricebond-checker");
+    const users = database.collection("users");
+
+    app.post("/user", async (req, res) => {
+      try {
+        const user = req.body;
+        console.log(user);
+      } catch (error) {}
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
