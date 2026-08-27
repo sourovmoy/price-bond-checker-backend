15b60c5b7bef19f448b14c500381c751d18265d7
sourovmmoysanju <sourovmmoysanju@gmail.com>
2026-08-27 13:48:23 +0600

    test commit 2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 import crypto from "crypto";
 
-dotenv.config();
+// dotenv.config();
 
 import express from "express";
 import cors from "cors";
